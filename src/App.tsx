bf82762eb72a628a0c7c3559dccca925a3cf7fe4
paslkal <164247200+paslkal@users.noreply.github.com>
2024-09-03 21:14:32 +0300

    add route in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './general.css'
 import Products from './Products'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return(
     <>
-      <Products/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/products' element={<Products/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
