f1914a5da2e3f5bf8425564d085bbe45522f8cd6
paslkal <164247200+paslkal@users.noreply.github.com>
2024-09-04 13:12:18 +0300

    get product from local storage instead of api

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Book, NewBook } from "./ProductTypes"
+import { loadFromStorage } from "./localStorage"
 
 function Product() {
+  // get product from API  
+  
+  /*
   const exampleBook = {
     number: 1,
     title: "Harry Potter and the Sorcerer's Stone",
@@ -33,6 +37,30 @@ function Product() {
 
     fetchData()
   }, [])
+
+  */
+
+  // get product from local storage
+  const {id} = useParams<{id: string}>()
+
+  const exampleBook: NewBook = {
+    number: 1,
+    title: "Harry Potter and the Sorcerer's Stone",
+    originalTitle: "Harry Potter and the Sorcerer's Stone",
+    releaseDate: "Jun 26, 1997",
+    description: "On his birthday, Harry Potter discovers that he is the son of two well-known wizards, from whom he has inherited magical powers. He must attend a famous school of magic and sorcery, where he establishes a friendship with two young men who will become his companions on his adventure. During his first year at Hogwarts, he discovers that a malevolent and powerful wizard named Voldemort is in search of a philosopher's stone that prolongs the life of its owner.",
+    pages: 223,
+    cover: "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/1.png",
+    index: 0,
+    isLiked: false
+  }
+
+  const books = loadFromStorage()
+
+  let book = books.find(book => book.index === Number(id))
+
+  if (!book) book = exampleBook
+
 
   return (
     <>
